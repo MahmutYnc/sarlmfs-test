250cfe8fb022b862e2e924de2f65d369da805a3e
Mahmut <mahmutla33@gmail.com>
2019-09-05 20:50:26 +0300

    origin

--- js/script.js
+++ js/script.js
@@ -40,3 +40,9 @@ $(function() {
     });
 
     $("[data-fancybox]").fancybox();
+
+    // mobile menu slide from the left
+$('[data-toggle="slide-collapse"]').on('click', function() {
+    $navMenuCont = $($(this).data('target'));
+    $navMenuCont.animate({'width':'toggle'}, 350);
+}
